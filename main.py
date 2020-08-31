5a779b5495cfed8765eee6bd8f7d4243d8823c49
Yoshida Nobufumi ICuTok <69101580+yoshida-nobufumi-icutok@users.noreply.github.com>
2020-09-01 06:09:19 +0900

    一部変更しました。

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+#これはテストです。
 def on_gesture_shake():
-    basic.show_number(randint(1, 6))
-    
-input.on_gesture(Gesture.Shake, on_gesture_shake)
+    rand_set = randint(1, 6)
+    basic.show_number(rand_set)
+    basic.pause(500)
+    basic.show_number(rand_set+1)
+    basic.clear_screen()
+
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+// これはテストです。
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    basic.showNumber(randint(1, 6))
+    let rand_set = randint(1, 6)
+    basic.showNumber(rand_set)
+    basic.pause(500)
+    basic.showNumber(rand_set + 1)
+    basic.clearScreen()
 })
